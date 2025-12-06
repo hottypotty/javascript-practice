2d856029ee5d5455678cfc556ed4b3bdb14b448f
Your Name <123456789+odin@users.noreply.github.com>
2025-12-06 18:49:51 +0800

    Java Script Project Initial Commit

---
+++ my-script.js
@@ -0,0 +1,43 @@
+
+// console.log("Hello, World!");
+
+// Declare two variables to store first and last names ( Input)
+// let firstName = "John";
+// let lastName = "Doe";
+
+// // Output the values of the variables
+// console.log(firstName);
+// console.log(lastName);
+
+
+// let age = 11;
+// console.log(age);
+
+// age = 54;
+// console.log(age);
+
+
+// const pi = 3.14;
+// pi = 10;
+
+// console.log(pi);
+
+// let me = (3+2)-76 * (1+1);
+// console.log(me);
+
+// console.log(23 + 97+30+100+9+1+40+39+5);
+
+// console.log((4+6+9) / 77);
+
+
+// let a = 10;
+// a = 69;
+// let b = 7 * a;
+// console.log(b);
+
+
+// const max = 57;
+// const actual = max - 13;
+// const percentage = actual / max;
+
+// console.log(percentage);
