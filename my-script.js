5b8882241162f11b11109b3c15654fa8732950c4
Your Name <123456789+odin@users.noreply.github.com>
2025-12-07 07:49:51 +0800

    Javascript Numbers

--- my-script.js
+++ my-script.js
@@ -130,9 +130,18 @@
 
 // Infinity Loop
 
-let myNumber = 2;
+// let myNumber = 2;
 
-while (myNumber != Infinity){
-    myNumber = myNumber * myNumber;
-    console.log(myNumber);
-}
+// while (myNumber != Infinity){
+//     myNumber = myNumber * myNumber;
+//     console.log(myNumber);
+// }
+
+
+let x = 2 / 0 ;
+let y = -2 / 0 ;
+
+
+
+// console.log(x);
+// console.log(y);
